440a0a548e371755b87066ef7821309cfad36600
rstyczynski <rstyczynski@gmail.com>
2020-05-25 11:47:02 +0200

    oci notification

--- bin/soa_servicestate.sh
+++ bin/soa_servicestate.sh
@@ -5,11 +5,23 @@ wls_env=$1
 wls_name=$2
 
 
+function oci_notification() {
+    local oci_notification_msg=$1
+
+    timeout 30 oci ons message publish --topic-id $oci_topic_id --body "$oci_notification_msg"
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        err_msg="Error sending OCI notification. Code: $exit_code"
+        echo $err_msg
+        loggger "$script_code: $err_msg"
+    fi
+}
+
 function stop() {
     error_code=$1
     [ -z "$error_code" ] && error_code=0
 
-    rm -rf /tmp/$$\_$caller
+    rm -rf /tmp/$$\_$script_code
     exit $error_code
 }
 
@@ -42,10 +54,13 @@ fi
 #
 #
 
-caller=soa_servicestate
-tmp=/tmp/$$\_$caller; mkdir -p $tmp
+script_code=soa_servicestate
+tmp=/tmp/$$\_$script_code; mkdir -p $tmp
 
+# clean up after ctrl-break
+TRAP stop INT
 
+# set / get parameters
 function getParameters() {
 
     # address
@@ -104,8 +119,22 @@ function getParameters() {
         fi
     fi
 
+    #error handler port
+    lookup_code=$(echo $(hostname)\_$wls_env\_$wls_name\_oci_topic_id)
+    oci_topic_id=$(cat ~/etc/soa.cfg | grep "$lookup_code" | tail -1 | cut -d= -f2 )
+    if [ -z "$oci_topic_id" ]; then
+        read -t 5 -p 'oci_topic_id:' oci_topic_id
+        if [ $? -ne 0 ]; then
+            echo 'Error: oci_topic_id not known and not privided.'
+            return 1
+        else
+            echo "$lookup_code=$oci_topic_id" >>~/etc/soa.cfg
+            chmod 600 ~/etc/soa.cfg
+        fi
+    fi
+
     # username
-    lookup_code=$(echo $(hostname)_$wls_env\_$wls_ip\_$wls_port\_user\_$caller | sha256sum | cut -f1 -d' ')
+    lookup_code=$(echo $(hostname)_$wls_env\_$wls_ip\_$wls_port\_user\_$script_code | sha256sum | cut -f1 -d' ')
     wls_user=$(cat ~/etc/secrets.cfg | grep "$lookup_code" | tail -1 | cut -d= -f2)
     if [ -z "$wls_user" ]; then
         read -t 5 -p 'wls_user:' wls_user
@@ -119,7 +148,7 @@ function getParameters() {
     fi
 
     # password
-    lookup_code=$(echo $(hostname)\_$wls_env\_$wls_ip\_$wls_port\_pass\_$caller | sha256sum | cut -f1 -d' ')
+    lookup_code=$(echo $(hostname)\_$wls_env\_$wls_ip\_$wls_port\_pass\_$script_code | sha256sum | cut -f1 -d' ')
     wls_pass=$(cat ~/etc/secrets.cfg | grep "$lookup_code" | tail -1 | cut -d= -f2)
     if [ -z "$wls_pass" ]; then
         read -t 5 -s -p 'wls_pass:' wls_pass
@@ -133,6 +162,7 @@ function getParameters() {
     fi
 }
 
+# send service state to CSF
 function reportCompositeDown() {
     composite_name=$1
 
@@ -168,12 +198,16 @@ EOF
     return $return_code
 }
 
+
+# get all parameters
 getParameters
 if [ $? -ne 0 ]; then
-    echo 'Error getting parameters. Service check not performed.'
+    err_msg='Error getting parameters. Service check not performed.'
+    echo $err_msg
     stop 2
 fi
 
+# invoke service check
 comp_file=$tmp/composites.txt
 rm -rf $comp_file
 timeout 30 $MW_HOME/oracle_common/common/bin/wlst.sh <<EOF
@@ -189,27 +223,43 @@ sys.stdout = old_stdout
 exit()
 EOF
 if [ $? -ne 0 ]; then
-    echo "Error starting WLST: $MW_HOME/oracle_common/common/bin/wlst.sh"
+    err_msg="Error starting WLST: $MW_HOME/oracle_common/common/bin/wlst.sh"
+    echo $err_msg
+    oci_notification "$err_msg"
     stop 3
 fi
 
 grep 'isDefault=true' $comp_file >/dev/null
 if [ $? -ne 0 ]; then
-    echo "Error getting service state. Service check not performed. Cause: $(cat $comp_file)"
+    err_msg="Error getting service state. Service check not performed. Cause: $(cat $comp_file)"
+    echo $err_msg
+    oci_notification "$err_msg"
     stop 4
 fi
 
+# report
 services_down=$(cat $comp_file | grep 'isDefault=true' | grep 'mode=active' | grep 'state=off' | cut -f2 -d' ' | cut -f1 -d, | sort)
-if [ "$(echo $services_down | wc -c)" -eq 0 ]; then
-    echo "All good. All services up."
+services_down_cnt=$(cat $comp_file | grep 'isDefault=true' | grep 'mode=active' | grep 'state=off' | cut -f2 -d' ' | cut -f1 -d, | sort | wc -l)
+if [ $services_down_cnt -eq 0 ]; then
+    err_msg="All good. All services up."
+    echo $err_msg
+    oci_notification "$err_msg"
 else
     echo "Services down. Sending notifications."
+    delivery_error=0
+    delivery_cnt=0
     for svc_name in $services_down; do
         reportCompositeDown $svc_name
-        if [ $? -ne 0 ]; then
-            echo "Error sending notification. "
+        if [ $? -eq 0 ]; then
+            delivery_ok=$(( $delivery_ok + 1 ))
+        else
+            err_msg="Error sending notification."
+            echo $err_msg
+            delivery_error=$(( $delivery_error + 1 ))
         fi
     done
+    err_msg="Services down. Discovered:$services_down_cnt, reported:$delivery_ok, not reported: $delivery_error"
+    oci_notification "$err_msg"
 fi
 
 echo "Done."
