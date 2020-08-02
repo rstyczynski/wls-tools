1902ddefab6bb8e750856e92d72378712e6e78d2
rstyczynski <rstyczynski@gmail.com>
2020-08-02 22:03:10 +0200

    .

--- bin/compare_hosts.sh
+++ bin/compare_hosts.sh
@@ -28,12 +28,12 @@ function compareHosts() {
 
     cd $left_domain_home
     find . -type d | 
-    sed "s/$left_instance/wls_insnce/g"
+    sed "s/$left_instance/wls_insnce/g" |
     sort >$tmp/dirs_left
     
     cd $right_domain_home
     find . -type d | 
-    sed "s/$right_instance/wls_insnce/g"
+    sed "s/$right_instance/wls_insnce/g" |
     sort >$tmp/dirs_right
 
     diff $tmp/dirs_left $tmp/dirs_right >$tmp/diff_dirs
