f49b851851adae3369eb6cbb8efe7746e4ea0a45
rstyczynski <rstyczynski@gmail.com>
2020-08-03 12:56:20 +0200

    .

--- bin/collect_wls_dumps.sh
+++ bin/collect_wls_dumps.sh
@@ -183,7 +183,7 @@ if [ $oswatcher == 'yes' ]; then
     echo ">> compressing oswatcher files..."
     if [ -f /etc/sysconfig/oswatcher ]; then
         osw_dir=$(grep "^DATADIR=" /etc/sysconfig/oswatcher | cut -f2 -d=)
-        cd $osw_dir/archive
+        cd $osw_dir
         tar -zcvf $debug_root/outbox/$collection_timestmap\_osw.tar.gz ./ >/dev/null
         cd -
     else
