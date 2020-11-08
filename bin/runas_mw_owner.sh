ed1e23bdd92d4255807d712a2854dc4ae1fde7ba
rstyczynski <rstyczynski@gmail.com>
2020-11-08 21:12:33 +0100

    run script as mw owner

--- bin/runas_mw_owner.sh
+++ bin/runas_mw_owner.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 source wls-tools/bin/discover_processes.sh 
+
+# to stop per from complains about locale
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 discoverWLS
 os_user=$(getWLSjvmAttr ${wls_managed[0]} os_user) 
 : ${os_user:=$(getWLSjvmAttr ${wls_admin[0]} os_user)}
@@ -9,6 +14,13 @@ if [ -z "$os_user" ]; then
     echo "Error. Oracle middleware not detected."
     exit 1
 else    
-    sudo su - $os_user $@
-    exit 0
+    if [ -f $1 ]; then
+        cp $1 /tmp/$$.$1
+        sudo su - $os_user $@
+        rm /tmp/$$.$1
+        exit 0
+    else
+        echo "Error. Script not found."
+        exit 2
+    fi
 fi
