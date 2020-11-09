62b8d7a75f6591b90d653e626871370c53abbcba
rstyczynski <rstyczynski@gmail.com>
2020-11-09 06:28:19 +0100

    .

--- bin/ansible_group.sh
+++ bin/ansible_group.sh
@@ -20,7 +20,7 @@ function usage() {
 group_names_raw=$(ansible -i $inventory $host -m debug -a 'var=group_names' -o)
 group_names_json=${group_names_raw#*=>}
 
-case $cmd in:
+case $cmd in
 count)
     group_cnt=$(echo $group_names_json | jq -r '.group_names' | tr -d '[\n] "' | tr , '\n' | wc -l)
     echo $group_cnt
