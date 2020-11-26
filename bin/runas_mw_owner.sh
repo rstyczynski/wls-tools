01b7f8df2e0d60716fb1c79d2eac6bbc282a05a3
rstyczynski <rstyczynski@gmail.com>
2020-11-26 12:00:42 +0100

    forward exec result

--- bin/runas_mw_owner.sh
+++ bin/runas_mw_owner.sh
@@ -33,13 +33,15 @@ else
 
         echo "Running as $mw_os_user: $script_to_run $@"
         sudo su - $mw_os_user $script_to_run "$@"
+        result=$?
         rm $script_to_run
-        exit 0
+        exit $result
     else
         invoke_cmd="$@"
         echo "Running as $mw_os_user: $invoke_cmd"
         echo "sudo su - $mw_os_user -c $invoke_cmd"
         sudo su - $mw_os_user -c "$invoke_cmd"
-        exit 0
+        result=$?
+        exit $result
     fi
 fi
