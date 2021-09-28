a3f860a62adaac54877594b6da19b8a8b91d7343
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2021-09-28 07:52:06 +0200

    unresponsive jcmd handler

--- bin/wls_jfr.sh
+++ bin/wls_jfr.sh
@@ -10,6 +10,16 @@ function usage() {
 "
 }
 
+unset jcmd_error_handler
+function jcmd_error_handler() {
+
+        echo "Error conecting to JVM. JFR interaction not possible. Writing thread dump instead."
+
+        file_name=$(hostname)_$wls_server\_$(date -u +"%Y-%m-%dT%H%M%S.000Z").jtop
+        $wls_tools_bin/wls_top.sh $wls_server > $dump_location/$file_name
+
+        echo "JVM thread dump written to $dump_location/$file_name."
+}
 
 unset wls_jfr
 function wls_jfr() {
@@ -67,6 +77,8 @@ function wls_jfr() {
     mkdir -p /tmp/$$
     tmp=/tmp/$$
 
+    wls_tools_bin=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
     mkdir -p $dump_location 
     if [ $dump_location == "$HOME/outbox/public" ]; then
         chmod 755 $HOME/outbox/public
@@ -80,63 +92,79 @@ function wls_jfr() {
     fi
 
     java_bin=$(dirname $(ps -o command ax -q $os_pid | grep -v 'COMMAND' | cut -f1 -d' '))
-    $java_bin/jcmd $os_pid VM.unlock_commercial_features >/dev/null
+    timeout 10 $java_bin/jcmd $os_pid VM.unlock_commercial_features >/dev/null
+    if [ $? -ne 0 ]; then
+        jcmd_error_handler
+    else
 
-    case $operation in
-    start)
-        $java_bin/jcmd $os_pid JFR.check >$tmp/jfr_check
-        recNo=$(grep Recording: $tmp/jfr_check | grep wls-tools_JFR | cut -d: -f2 | cut -d= -f2 | cut -d' ' -f1 | head -1)
-        if [ -z "$recNo" ]; then
-
-            file_name=$(hostname)_$wls_server\_$(date -u +"%Y-%m-%dT%H%M%S.000Z").jfr
-            $java_bin/jcmd $os_pid JFR.start name=wls-tools_JFR duration=$duration filename=$dump_location/$file_name compress=true
-            if [ $? -eq 0 ]; then
-                echo "Started $duration long recording. Output file will be written to $dump_location/$file_name"
-
-                echo
-
-                host_ip=$(hostname -i)
-                echo "Use scp to get recording: "
-                echo 
-                echo "scp -o \"ProxyJump \$user@\$jumpserver\" \$user@$host_ip:$dump_location/$file_name ."
-                echo 
-                echo "Once collected open with Java Mission Control - jmc / jmc.exe"
-                
-                # wait for file and make readable for all
-                while [ ! -f $dump_location/$file_name ]; do
-                    echo "waiting for file..."
-                    sleep 1
-                done
-
-                chmod o+r $dump_location/$file_name 
-                chmod g+r $dump_location/$file_name 
+        case $operation in
+        start)
+            timeout 10 $java_bin/jcmd $os_pid JFR.check >$tmp/jfr_check
+            if [ $? -ne 0 ]; then
+                jcmd_error_handler
             else
-                echo "Error starting recording."
-            fi
-        else
-            echo "Recording already started: $(grep Recording: $tmp/jfr_check)"
-        fi
-        ;;
-    stop)
-        $java_bin/jcmd $os_pid JFR.check >$tmp/jfr_check
-        recNo=$(grep Recording: $tmp/jfr_check | cut -d: -f2 | cut -d= -f2 | cut -d' ' -f1 | head -1)
-        if [ ! -z "$recNo" ]; then
-            file_name=$(hostname)_$wls_server\_$(date -u +"%Y-%m-%dT%H:%M:%S.000Z").jfr
-            $java_bin/jcmd $os_pid JFR.stop recording=$recNo
-            if [ $? -eq 0 ]; then
-                echo "Recording stopped."
-            else
-                echo "Error stopping recording."
-            fi
-        else
-            echo "No active recording."
-        fi
-        ;;
-    check)
-        $java_bin/jcmd $os_pid JFR.check
-        ;;
+                recNo=$(grep Recording: $tmp/jfr_check | grep wls-tools_JFR | cut -d: -f2 | cut -d= -f2 | cut -d' ' -f1 | head -1)
+                if [ -z "$recNo" ]; then
 
-    esac
+                    file_name=$(hostname)_$wls_server\_$(date -u +"%Y-%m-%dT%H%M%S.000Z").jfr
+                    timeout 10 $java_bin/jcmd $os_pid JFR.start name=wls-tools_JFR duration=$duration filename=$dump_location/$file_name compress=true
+                    if [ $? -eq 0 ]; then
+                        echo "Started $duration long recording. Output file will be written to $dump_location/$file_name"
+
+                        echo
+
+                        host_ip=$(hostname -i)
+                        echo "Use scp to get recording: "
+                        echo 
+                        echo "scp -o \"ProxyJump \$user@\$jumpserver\" \$user@$host_ip:$dump_location/$file_name ."
+                        echo 
+                        echo "Once collected open with Java Mission Control - jmc / jmc.exe"
+                        
+                        # wait for file and make readable for all
+                        while [ ! -f $dump_location/$file_name ]; do
+                            echo "waiting for file..."
+                            sleep 1
+                        done
+
+                        chmod o+r $dump_location/$file_name 
+                        chmod g+r $dump_location/$file_name 
+                    else
+                        echo "Error starting recording. Writing thread dump instead."
+                        jcmd_error_handler
+                    fi
+                else
+                    echo "Recording already started: $(grep Recording: $tmp/jfr_check)"
+                fi
+            fi
+            ;;
+        stop)
+            timeout 10 $java_bin/jcmd $os_pid JFR.check >$tmp/jfr_check
+            if [ $? -ne 0 ]; then
+                jcmd_error_handler
+            else
+                recNo=$(grep Recording: $tmp/jfr_check | cut -d: -f2 | cut -d= -f2 | cut -d' ' -f1 | head -1)
+                if [ ! -z "$recNo" ]; then
+                    file_name=$(hostname)_$wls_server\_$(date -u +"%Y-%m-%dT%H:%M:%S.000Z").jfr
+                    timeout 10 $java_bin/jcmd $os_pid JFR.stop recording=$recNo
+                    if [ $? -eq 0 ]; then
+                        echo "Recording stopped."
+                    else
+                        echo "Error stopping recording. Writing thread dump instead."
+                        jcmd_error_handler
+                    fi
+                else
+                    echo "No active recording."
+                fi
+            fi
+            ;;
+        check)
+            timeout 10 $java_bin/jcmd $os_pid JFR.check
+            if [ $? -ne 0 ]; then
+                jcmd_error_handler
+            fi
+            ;;
+        esac
+    fi
 
     # todo
     if [ ! "$opt" == debug ]; then
