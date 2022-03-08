e4056818e6869bf2d6b33b3ced201a452fe3ade7
Ryszard Styczynski <ryszard.styczynski@oracle.com>
2022-03-08 16:21:22 +0100

    .

--- bin/nm_process_start.sh
+++ bin/nm_process_start.sh
@@ -8,11 +8,11 @@ log_name=$4
 if [ $(whoami) != $DOMAIN_OWNER ]; then
     file_no=$(sudo su $DOMAIN_OWNER -c "ls $log_dir | grep -P '$log_name\.out\.\d+' | cut -d. -f3 | sort -nr | head -1")
 else
-    file_no=$(ls $log_dir | grep -P "$log_dir\.out\.\d+" | cut -d. -f3 | sort -nr | head -1)
+    file_no=$(ls $log_dir | grep -P "$log_name\.out\.\d+" | cut -d. -f3 | sort -nr | head -1)
 fi
 : ${file_no:=0}
-
 file_no=$(( $file_no + 1 ))
+
 stdout_log=$log_dir/$log_name.out.$file_no
 stderr_log=$log_dir/$log_name.err.$file_no
 
